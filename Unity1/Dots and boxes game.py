94dcf2e51133fdb45e518b69daa2112f9afa2153
Eric Pei <spei@bayviewglen.ca>
2023-10-10 13:13:36 -0400

    fString formatting Examples

---
+++ Unity1/Dots and boxes game.py
@@ -0,0 +1,7 @@
+name = str(input("What is your name?"))
+# Name Of Opponent
+noo_1 = str(input("Who is your opponent for the first game?"))
+# Number of boxes you created
+nby_1 = int(input("How many boxes did you create in the first game?"))
+# Number of boxes opponent created
+nbo_1 = int(input("How many boxes did your opponent create in the first game?"))
