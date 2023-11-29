eb19c525c04f419130fcaf7fd4ae31be346d7815
Eric Pei <spei@bayviewglen.ca>
2023-11-28 21:02:49 -0500

    Pokemon game but better

--- Unity1/Pokemon_Game.py
+++ Unity1/Pokemon_Game.py
@@ -33,23 +33,63 @@ def make_decision():
         print("Invalid decision. Please try again.")
         return make_decision()
 
-# Task 4: Pokemon Battle
+# Task 4: Pokemon Battle + Task 6: Element reaction
 def pokemon_battle(action, player_pokemon):
     if action == "battle":
         opponent_pokemon = random.choice(["Squirtle", "Bulbasaur", "Charmander"])
         print(f"You are battling a wild {opponent_pokemon} with your {player_pokemon}!")
         
-        player_attack = random.randint(5, 15)  # Simulate player's attack damage
-        opponent_attack = random.randint(5, 15)  # Simulate opponent's attack damage
-        
         player_health = 100  # Set player's initial health
+        player_energy = 5 # Set player's energy level
         opponent_health = 100  # Set opponent's initial health
-        
+
         while player_health > 0 and opponent_health > 0:
             # Player's turn
-            print(f"Your {player_pokemon} attacks the wild {opponent_pokemon}!")
-            opponent_health -= player_attack
-            print(f"The wild {opponent_pokemon}'s health: {opponent_health}")
+            player_choice = input("Choose your attack (e.g., punch, kick, special move, defense): ")
+            if player_choice == "punch" and player_energy >=1:
+                player_attack = random.randint(5, 10)  # Simulate player's punch attack damage
+                player_energy -= 1
+                if player_pokemon == "Pikachu" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Pikachu" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 5
+                elif player_pokemon == "Bulbasaur" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Charmander" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 10
+                opponent_health -= player_attack
+            elif player_choice == "kick" and player_energy >= 2:
+                player_attack = random.randint(10, 15)  # Simulate player's kick attack damage
+                player_energy -= 2
+                if player_pokemon == "Pikachu" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Pikachu" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 5
+                elif player_pokemon == "Bulbasaur" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Charmander" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 10
+                opponent_health -= player_attack
+            elif player_choice == "special move" and player_energy >= 4:
+                player_attack = random.randint(0, 30)  # Simulate player's special move attack damage
+                player_energy -= 4
+                if player_pokemon == "Pikachu" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Pikachu" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 5
+                elif player_pokemon == "Bulbasaur" and opponent_pokemon == "Squirtle":
+                    player_attack += 5
+                elif player_pokemon == "Charmander" and opponent_pokemon == "Bulbasaur":
+                    player_attack += 10
+                opponent_health -= player_attack
+            elif player_choice == "defense" and player_energy < 9:
+                player_energy += 2
+                opponent_health += 10
+                print(f"You defensed yourself, your {player_pokemon}'s health: {player_health}, opponent's health: {opponent_health}, your {player_pokemon}'s energy level: {player_energy}.")
+                continue
+            else:
+                print("Invalid attack choice. Please choose a valid attack.")
+                continue  # Restart the loop to allow the player to choose a valid attack
             
             if opponent_health <= 0:
                 print(f"You defeated the wild {opponent_pokemon}! Congratulations!")
@@ -58,8 +98,15 @@ def pokemon_battle(action, player_pokemon):
             
             # Opponent's turn
             print(f"The wild {opponent_pokemon} counterattacks!")
+            opponent_attack = random.randint(15, 20)
+            if player_pokemon == "Pikachu" and opponent_pokemon == "Charmander":
+                opponent_attack += 5
+            elif player_pokemon == "Bulbasaur" and opponent_pokemon == "Charmander":
+                opponent_attack += 10
+            elif player_pokemon == "Charmander" and opponent_pokemon == "Squirtle":
+                opponent_attack += 5
             player_health -= opponent_attack
-            print(f"Your {player_pokemon}'s health: {player_health}")
+            print(f"Your {player_pokemon}'s health: {player_health}, opponent's health: {opponent_health}, your {player_pokemon}'s energy level: {player_energy}.")
             
             if player_health <= 0:
                 print(f"Your {player_pokemon} fainted! Game over.")
